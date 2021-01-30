a8d61b4823b3cc168bcb5b6175e500356d723f43
Nicholas Reid <nicholasreid845@gmail.com>
2021-01-30 14:44:53 -0500

    ready for production

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const POKEDEX = require('./pokedex.json')
 
 const app = express()
 
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors())
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-    console.log('validate bearer token middleware')
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
@@ -48,8 +49,18 @@ function handleGetPokemon(req, res) {
 
 app.get('/pokemon', handleGetPokemon)
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error'}}
+    } else {
+        response = { error }
+    }
 
-app.listen(PORT, () => {
-    console.log(`Server listening at ${PORT}`)
+    res.status(500).json(response)
 })
+
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT)
